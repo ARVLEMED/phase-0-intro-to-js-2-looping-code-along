7444dabb2e2a8da301f1038bd9e30aaab2822144
aron k koech <varokoaruna81@gmail.com>
2024-10-09 12:21:31 +0300

    adds looping functions

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const { Context } = require("mocha");
+
+const ar=["Guadalupe","Ollie","Aki"]
+function writeCards(ar, eventName){
+    let messages=[];
+    for (let i=0;i<ar.length; i++) {
+    messages.push (`Thank you, ${ar[i]}, for the wonderful ${eventName} gift!`)
+    }
+return messages;
+}
+
+function countDown(k) {
+    let countDown=k;
+    while (countDown>= 0){
+        console.log(countDown--)
+    }
+}
